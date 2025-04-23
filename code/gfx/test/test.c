8706d33149320b10ee504974dd1349785876834f
Axel Hallsenius <axelhallsenius@gmail.com>
2025-04-23 16:20:19 +0200

    tests

---
+++ code/gfx/test/test.c
@@ -0,0 +1,20 @@
+#include "test.h"
+
+tm_ellipsoid grs80 = {
+  6378137.0000,
+  1/298.257222101,
+  13.58547,
+  1.000002540000,
+  -6226307.8640,
+  84182.8790
+};
+
+void test_geod_grid(SDL_Renderer *rend, SDL_FRect *rect){
+  point_tm_grid gridp;
+  point_geod geop;
+  geop.deg_lat = 0.0;
+  geop.deg_long = 0.0;
+  gridp = geod_to_tm_grid(grs80, geop);
+  draw_example_point_tm(rend, rect, gridp.x, gridp.y);
+
+}
---
+++ code/gfx/test/test.h
@@ -0,0 +1,16 @@
+#include "../shapes/shapes.h"
+#include "../shapes/map.h"
+
+#ifndef TEST_H
+#define TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void test_geod_grid(SDL_Renderer *rend, SDL_FRect *rect);
+
+#endif
+#ifdef __cplusplus
+}
+#endif
