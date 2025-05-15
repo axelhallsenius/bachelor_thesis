c9bb82257748b4b9b11cf68a601599445cdf2f4a
Axel Hallsenius <axelhallsenius@gmail.com>
2025-05-15 16:02:19 +0200

    vinc test init

---
+++ code/tests/unit/test_vincenty.cpp
@@ -0,0 +1,23 @@
+#include <limits.h>
+#include "gtest/gtest.h"
+#include "vincenty.h"
+
+namespace {
+
+//Distance
+TEST(VincentyInverse, Distance1){
+  //TODO
+}
+TEST(VincentyInverse, Distance2){
+}
+TEST(VincentyInverse, Distance3){
+}
+
+TEST(VincentyInverse, Location){
+}
+
+//Location same as Gauss Kruger?
+TEST(VincentyDirect, CompareGK){
+}
+
+}
